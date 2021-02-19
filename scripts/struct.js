4f4819475f4a18e333b630b0d995b78f6a2c2380
James <moverinfo@gmail.com>
2021-02-19 19:40:14 +0800

    Add setCursorToEndOf().

--- scripts/struct.js
+++ scripts/struct.js
@@ -305,3 +305,32 @@ struct.getCaretPosition = function(editableDiv) {
    }
    return caretPos;
 }
+
+struct.setCursorToEndOf = function (editableDiv)
+{
+   var childNodes = editableDiv.childNodes;
+   for(i=0;i<childNodes.length;i++) {
+        child = childNodes[i];
+        if (child.tagName && child.tagName === "BR") {
+                   editableDiv.removeChild(child);
+        }
+   }
+
+   var range,selection;
+   if(document.createRange)//Firefox, Chrome, Opera, Safari, IE 9+
+   {
+       range = document.createRange();//Create a range (a range is a like the selection but invisible)
+       range.selectNodeContents(editableDiv);//Select the entire contents of the element with the range
+       range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
+       selection = window.getSelection();//get the selection object (allows you to change selection)
+       selection.removeAllRanges();//remove any selections already made
+       selection.addRange(range);//make the range you have just created the visible selection
+   }
+   else if(document.selection)//IE 8 and lower
+   {
+       range = document.body.createTextRange();//Create a range (a range is a like the selection but invisible)
+       range.moveToElementText(editableDiv);//Select the entire contents of the element with the range
+       range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
+       range.select();//Select the range (make it the visible selection
+   }
+}
