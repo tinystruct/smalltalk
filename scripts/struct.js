3b410aa97836ac07ca93fb3e3169316c4c54d260
James <moverinfo@gmail.com>
2021-02-19 16:46:25 +0800

    Add caret position support.

--- scripts/struct.js
+++ scripts/struct.js
@@ -281,3 +281,27 @@ struct.getStyle = function(className, attr) {
 
 	return value;
 }
+
+struct.getCaretPosition = function(editableDiv) {
+   var caretPos = 0, sel, range;
+   if (window.getSelection) {
+     sel = window.getSelection();
+     if (sel.rangeCount) {
+       range = sel.getRangeAt(0);
+       if (range.commonAncestorContainer.parentNode == editableDiv) {
+         caretPos = range.endOffset;
+       }
+     }
+   } else if (document.selection && document.selection.createRange) {
+     range = document.selection.createRange();
+     if (range.parentElement() == editableDiv) {
+       var tempEl = document.createElement("span");
+       editableDiv.insertBefore(tempEl, editableDiv.firstChild);
+       var tempRange = range.duplicate();
+       tempRange.moveToElementText(tempEl);
+       tempRange.setEndPoint("EndToEnd", range);
+       caretPos = tempRange.text.length;
+     }
+   }
+   return caretPos;
+}
