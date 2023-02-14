b990c169e5a3013d00f63bbeaea05e0b5b19e3a3
James ZHOU <moverinfo@gmail.com>
2023-02-14 23:26:47 +0800

    Add OpenJDK installation script.

---
+++ bin/installer.cmd
@@ -0,0 +1,6 @@
+@echo off
+set "ROOT=%~dp0..\"
+CALL %ROOT%\bin\dispatcher.cmd download --url https://www.7-zip.org/a/7zr.exe
+CALL %ROOT%\bin\dispatcher.cmd download --url https://download.java.net/java/GA/jdk17/0d483333a00540d886896bac774ff48b/35/GPL/openjdk-17_windows-x64_bin.zip && a\7zr.exe X java\GA\jdk17\0d483333a00540d886896bac774ff48b\35\GPL\openjdk-17_windows-x64_bin.zip
+setx JAVA_HOME "%ROOT%\jdk-17"
+echo OpenJDK17 has installed successfully.
